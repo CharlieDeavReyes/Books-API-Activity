84753c169573acddcf7d74ecb0496cee4e121796
charlie reyes <charliereyes1228@gmail.com>
2022-10-13 16:20:07 -0700

    added dependencies cors

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-// Require needed modules.
+// DEPENDENCIES
 const express = require('express')
-
-// Initialize the app object.
+const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express()
 
-// Create a homepage route.
-app.get('/', function (req, res) {
-    // This gets sent to the client
-    // (your web browser most likely!)
-    res.send('Hello world')
+// CONFIG/MIDDLEWARE
+require('dotenv').config()
+const PORT = process.env.PORT
+app.use(express.json())
+app.use(cors())
+
+// MONGOOSE
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('connected to mongo on: ', process.env.MONGO_URI)
 })
 
-// Listen for connections.
-app.listen(3000)
+// ROOT INDEX 
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
+// BOOKS 
+const booksController = require('./controllers/books_controller')
+app.use('/books', booksController)
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+})
